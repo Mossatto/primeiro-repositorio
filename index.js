dbf0a87b62612775075e615453e9bb98be34ef55
Mossatto <matheusmossatto.dosantos@gmail.com>
2022-09-16 16:57:42 -0300

    atualização

---
+++ index.js
@@ -0,0 +1,6 @@
+a=2 ;
+b=3 ;
+c=a+b;
+console.log(c);
+let intro = "hello world"
+console.log(intro);
---
+++ operadores_de_incremento.js
@@ -0,0 +1,4 @@
+alert('Código Funcionando')
+const nomeUsuario= prompt('Hello, user ! What your name ?')
+let num =Number(prompt('olá, ' + nomeUsuario + " write any number, please"));
+console.log(typeof num);
